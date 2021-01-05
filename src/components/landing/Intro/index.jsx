ae95f60f052e39552b8405c5460282bbc15ea53d
paulinemss <masse.pauline@gmail.com>
2021-01-05 15:56:25 +0100

    first commit

---
+++ src/components/landing/Intro/index.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { ThemeContext } from 'providers/ThemeProvider';
+import { Header } from 'components/theme';
+import { Container, Button } from 'components/common';
+import mainPauline from 'assets/illustrations/mainPauline.png';
+import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles';
+
+export const Intro = () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <Wrapper>
+      <Header />
+      <IntroWrapper as={Container}>
+        <Details theme={theme}>
+          <h1>Hi there, I'm Pauline.</h1>
+          <h4>Passionate about diverse and inclusive technology, I'm available for hire.</h4>
+          <Button as={AnchorLink} href="#contact">
+            Contact me
+          </Button>
+        </Details>
+        <Thumbnail>
+          <img src={mainPauline} alt="Pauline Massé, frontend developer" />
+        </Thumbnail>
+      </IntroWrapper>
+    </Wrapper>
+  );
+};
