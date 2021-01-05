b3476f45b9857e621e44d05b88c0d0496ad22175
paulinemss <masse.pauline@gmail.com>
2021-01-05 16:57:43 +0100

    add favicons

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -7,6 +7,7 @@ import ContactForm from './ContactForm';
 export const Contact = () => (
   <Wrapper as={Container} id="contact">
     <Details>
+      <h1>Contact me</h1>
       <ContactForm />
     </Details>
     <Thumbnail>
